cab369de30112e41f99399589d316c402a485e6b
jmn <norlund@gmail.com>
2017-06-17 10:32:24 +0200

    uses hpack

--- src/Trash.hs
+++
@@ -1,7 +0,0 @@
--- | Stuff that might be worth keeping around for later use.
-
-toHeaderName :: String -> HeaderName
-toHeaderName header = CI.mk (BytCh.pack header)
-
-respInfo :: Response L8.ByteString -> IO ()
-respInfo resp = print $ getResponseHeader (toHeaderName "content-type") resp
